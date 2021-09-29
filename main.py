3fd84b36888147a5ff4069f7c610609899ad96f7
ZarnescuBogdan <zarnescu_bogdan@yahoo.com>
2021-09-30 01:13:32 +0300

    Am reusit

--- main.py
+++ main.py
@@ -2,77 +2,57 @@
 Returneaza true daca n este prim si false daca nu.
 '''
 def is_prime(n):
+  '''
+  '''
   # codul vostru aici
-    int(n,k,ok)
+    n = int(input("Dati un numar: "))
     k = 2
     ok = 0
-    while k < n:
-    {
-        if n % k == 0
-        {
-            ok = 1
-            break
-        }
-        k++
-    }
-    if ok == 1
-    {
-      print("n = ")
-      print(n)
-      print(" nu este prim")
-      return True
-    }
-    else
-    {
-      print("n = ")
-      print(n)
-      print(" este prim")
+    if n < 2:
       return False
-    }
+    while k < n/2:
+      if n % k == 0:
+        return False
+      k++
+    return True
   
 '''
 Returneaza produsul numerelor din lista lst.
 '''
 def get_product(lst):
   # codul vostru aici
-    int(n,p)
+    n = int(input("Dati numarul de numere: "))
     p = 1
-    int(v[n+1],i)
-    for i in range(1,n):
-      {
-        p *= v[i]
-      }
-    print("produsul este ")
-    print(p)
+    for i in range(n):
+      x = int(input("x = "))
+      p = p*x
+    return p
   
 '''
 Returneaza CMMDC a doua numere x si y folosind primul algoritm.
 '''
 def get_cmmdc_v1(x, y):
   # codul vostru aici
-  int(a,b)
-    while a != b:
-    {
-        if a < b
-            b -= a
-        else
-            a -= b
-    }
-    print(a)
-  
+  x = int(input("x = "))
+  y = int(input("y = "))
+  while x != y:
+    if x < y
+      y = y-x
+    else
+      x = x-y
+  return x
 '''
 Returneaza CMMDC a doua numere x si y folosind al doilea algoritm.
 '''
 def get_cmmdc_v2(x, y):
   # codul vostru aici
-  int(a,b,r)
-    while b != 0:
-    {
-        r = a%b
-        a = b
-        b = r
-    }
-    print(a)
+  x = int(input("x = "))
+  y = int(input("y = "))
+  while b != 0:
+    r = a%b
+    a = b
+    b = r
+  return a
   
 def main():
   # interfata de tip consola aici
